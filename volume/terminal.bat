59dc52a5f25af1dce54ed76b8318a050582e981a
Toke Jepsen <tokejepsen@gmail.com>
2018-06-08 17:45:27 +0200

    Enable terminal to use Avalon python and modules.

--- volume/terminal.bat
+++ volume/terminal.bat
@@ -1,2 +1,4 @@
-set PATH=%~dp0;%PATH%
+set AVALON_MONGO=mongodb://192.168.99.100:27017
+set PATH=%~dp0;%~dp0bin\windows\python36;%PATH%
+set PYTHONPATH=%~dp0git\avalon-core;%~dp0git\avalon-launcher;%~dp0git\mindbender-config;%~dp0git\pyblish-base;%~dp0git\pyblish-qml;%PYTHONPATH%
 call cmd

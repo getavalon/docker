04e3d63c6836424f558aef90a8664d1bc0eebe93
Toke Jepsen <tokejepsen@gmail.com>
2018-06-20 13:55:05 +0200

    Switch to nose for testing.

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-import pytest
+import nose
 
 
 root = os.path.abspath(os.path.join(__file__, ".."))
@@ -13,6 +13,6 @@ os.environ["PATH"] += os.pathsep + os.path.join(root, "volume")
 subprocess.call(["docker", "build", ".", "-t", "getavalon/docker"], cwd=root)
 
 # Run tests
-pytest.main(
+nose.main(
     [os.path.abspath(os.path.join(__file__, "..", "tests"))]
 )

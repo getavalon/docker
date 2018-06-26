5c32476250eaa355e7d8664076b49723525bda1e
Toke Jepsen <tokejepsen@gmail.com>
2018-06-26 19:47:17 +0200

    Change extension

--- volume/avalon.sh
+++ volume/avalon.sh
@@ -3,4 +3,4 @@ pushd `dirname $0` > /dev/null
 DIRNAME=`pwd -P`
 popd > /dev/null
 export AVALON_MONGO=mongodb://192.168.99.100:27017
-python -u $DIRNAME/avalon_cli.py "$*"
+python -u $DIRNAME/avalon_cli.py $*

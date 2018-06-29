6b975ee108fde958e442998a833c5e7cf8e58c8a
Marcus Ottosson <konstruktion@gmail.com>
2018-06-29 09:27:34 +0100

    Support sourcing of Linux terminal script

--- volume/terminal.sh
+++ volume/terminal.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-pushd `dirname $0` > /dev/null
-DIRNAME=`pwd -P`
-popd > /dev/null
+
+if [[ -z $WORKDIR ]]; then
+	pushd `dirname $0` > /dev/null
+	WORKDIR=`pwd -P`
+	popd > /dev/null
+fi
 
 export AVALON_MONGO=mongodb://127.0.0.1:27017
-export PATH=$DIRNAME:$PATH
+export PATH=$WORKDIR:$PATH
 
 # Expose Python libraries
-export PYTHONPATH=$DIRNAME/git/avalon-core
-export PYTHONPATH=$DIRNAME/git/avalon-launcher:$PYTHONPATH
-export PYTHONPATH=$DIRNAME/git/mindbender-config:$PYTHONPATH
-export PYTHONPATH=$DIRNAME/git/pyblish-base:$PYTHONPATH
-export PYTHONPATH=$DIRNAME/git/pyblish-qml:$PYTHONPATH
-export PYTHONPATH=$DIRNAME/git/cgwire-gazu:$PYTHONPATH
+export PYTHONPATH=$WORKDIR/git/avalon-core
+export PYTHONPATH=$WORKDIR/git/avalon-launcher:$PYTHONPATH
+export PYTHONPATH=$WORKDIR/git/mindbender-config:$PYTHONPATH
+export PYTHONPATH=$WORKDIR/git/pyblish-base:$PYTHONPATH
+export PYTHONPATH=$WORKDIR/git/pyblish-qml:$PYTHONPATH
+export PYTHONPATH=$WORKDIR/git/cgwire-gazu:$PYTHONPATH
 
 # Expose cross-platform libraries
-export PYTHONPATH=$DIRNAME/bin/pythonpath:$PYTHONPATH
+export PYTHONPATH=$WORKDIR/bin/pythonpath:$PYTHONPATH

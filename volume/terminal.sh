214db57025a6bfa33d152978ddedc0ceec8b3489
Toke Jepsen <tokejepsen@gmail.com>
2018-07-18 22:21:05 +0200

    Fix environment variables

--- volume/terminal.sh
+++ volume/terminal.sh
@@ -7,11 +7,13 @@ if [[ -z $WORKDIR ]]; then
 fi
 
 export AVALON_MONGO=mongodb://127.0.0.1:27017
+export AVALON_CORE=$WORKDIR/git/avalon-core
+export AVALON_LAUNCHER=$WORKDIR/git/avalon-launcher
 export PATH=$WORKDIR:$PATH
 
 # Expose Python libraries
-export PYTHONPATH=$WORKDIR/git/avalon-core
-export PYTHONPATH=$WORKDIR/git/avalon-launcher:$PYTHONPATH
+export PYTHONPATH=$AVALON_CORE
+export PYTHONPATH=$AVALON_LAUNCHER:$PYTHONPATH
 export PYTHONPATH=$WORKDIR/git/mindbender-config:$PYTHONPATH
 export PYTHONPATH=$WORKDIR/git/pyblish-base:$PYTHONPATH
 export PYTHONPATH=$WORKDIR/git/pyblish-qml:$PYTHONPATH

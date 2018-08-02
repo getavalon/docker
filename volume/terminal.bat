11e4f51185f935a9d91fa63ea5085f09ed131da2
Marcus Ottosson <marcus@weightshift.io>
2018-08-02 09:09:15 +0100

    Consolidate .bat files

--- volume/terminal.bat
+++ volume/terminal.bat
@@ -1,31 +1,35 @@
 @echo off
 
+set AVALONCACHE=%HOMEDRIVE%%HOMEPATH%\.avalon
+set PATH=%AVALONCACHE%\bin\python36;%PATH%
+
+if not exist "%AVALONCACHE%" (
+    echo Avalon was started for the first time, hold on..
+    %~dp0bin\windows\python36\python.exe %~dp0avalon_cli.py --firsttime
+    sleep 1
+)
+
+if not exist "%AVALONCACHE%" (
+    echo This is a bug
+    exit /b
+)
+
 set AVALON_MONGO=mongodb://192.168.99.100:27017
-set PATH=%~dp0;%~dp0bin\windows\python36;%PATH%
-
-:: Expose Python libraries
-set PYTHONPATH=%~dp0git\avalon-core
-set PYTHONPATH=%~dp0git\avalon-launcher;%PYTHONPATH%
-set PYTHONPATH=%~dp0git\mindbender-config;%PYTHONPATH%
-set PYTHONPATH=%~dp0git\pyblish-base;%PYTHONPATH%
-set PYTHONPATH=%~dp0git\pyblish-qml;%PYTHONPATH%
-set PYTHONPATH=%~dp0git\cgwire-gazu;%PYTHONPATH%
-
-:: Expose cross-platform libraries
-set PYTHONPATH=%~dp0bin\pythonpath;%PYTHONPATH%
 
 cls
 
-echo.
-echo  Avalon Terminal
-echo  ---------------
-echo.
-echo  :: Launch avalon
-echo  $ avalon
-echo.
-echo  :: Get help
-echo  $ avalon --help
-echo.
-echo.
+if "%1" == "" (
+    echo.
+    echo  Avalon Terminal
+    echo  ---------------
+    echo.
+    echo  :: Launch avalon
+    echo  $ avalon
+    echo.
+    echo  :: Get help
+    echo  $ avalon --help
+    echo.
+    echo.
+)
 
-call cmd /K
+call cmd /K %*

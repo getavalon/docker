ce2711ca820ea383b37f1bf3b649fa4af72d2f8d
Toke Jepsen <tokejepsen@gmail.com>
2018-08-03 23:00:25 +0200

    Add cross-platform libraries for PYTHONPATH.

--- volume/terminal.bat
+++ volume/terminal.bat
@@ -3,6 +3,9 @@
 set AVALON_MONGO=mongodb://192.168.99.100:27017
 set PATH=%~dp0;%~dp0bin\windows\python36;%PATH%
 
+:: Expose cross-platform libraries
+set PYTHONPATH=%~dp0bin\pythonpath;%PYTHONPATH%
+
 cls
 
 echo.
